29230c0de87a33d61953fac70e7ace48c7eb6a8b
HelloCodeman <955khm@gmail.com>
2023-11-30 16:03:34 +0800

    完成編輯文字及設定是否顯示的功能

--- q1/edit_title.php
+++ q1/edit_title.php
@@ -1,11 +1,28 @@
 <?php
 include_once "db.php";
 dd($_POST);
+foreach ($_POST['id'] as $key => $id) {
+    $row = $Title->find($id);
+    // dd($row);
+    $row['text'] = $_POST['text'][$key];
+    // dd($row);
+    $Title->save($row);
+}
 
+foreach ($_POST['id'] as $id) {
+    $row = $Title->find($id);
+    /* if($id==$_POST['sh']){
+        $row['display']=1;
+    }else{
+        $row['display']=0;
+    } */
+    $row['display'] = ($id == $_POST['display']) ? 1 : 0;
+    $Title->save($row);
+}
 /* 
-$_POST['sh']=0;
+$_POST['display']=0;
 $Title->save($_POST);
 header("location:index.php");
  */
 
-?>
+ ?>
