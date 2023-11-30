a2be5292def40eea1b7a0ae1f5534d088ab71003
HelloCodeman <955khm@gmail.com>
2023-11-30 16:32:33 +0800

    完成編輯文字及設定是否顯示的功能

--- q1/index.php
+++ q1/index.php
@@ -1,12 +1,11 @@
 <?php include_once "db.php"; ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>題組一</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../css/css.css">
 </head>
@@ -14,7 +13,8 @@
 <body>
     <div id="cover" style="display:none; ">
         <div id="coverr">
-            <a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;" onclick="cl('#cover')">X</a> <!-- cl=close/&#39;='/#=id -->
+            <a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;" onclick="cl('#cover')">X</a>
+            <!-- cl=close/&#39;='/#=id -->
             <div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
         </div>
     </div>
@@ -25,7 +25,7 @@
         <h3 class="text-center">網站標題管理</h3>
         <hr>
         <form action="edit_title.php" method="post">
-            <table class="table table-bordered text-center">
+            <table class='table table-bordered text-center'>
                 <tr>
                     <td>網站標題</td>
                     <td>替代文字</td>
@@ -39,18 +39,19 @@
                 ?>
                     <tr>
                         <td><img src="./img/<?= $row['img']; ?>" style="width:300px;height:30px"></td>
-                        <td><input type="text" name="text[<?= $row['id']; ?>]" id="" value="<?= $row['text']; ?>" style="width:90%"></td>
-                        <td><input type="radio" name="display" id="" value="<?= $row['id']; ?>" <?= ($row['display'] == 1) ? 'checked' : ''; ?>></td>></td>
-                        <td><input type="checkbox" name="del[<?= $row['id']; ?>]" id=""></td>
-                        <td><input class="btn btn-primary" type="button" value="更新圖片" name="" id=""></td>
+                        <td><input type="text" name="text[]" id="" value="<?= $row['text']; ?>" style="width:90%"></td>
+                        <td><input type="radio" name="display" id="" value="<?= $row['id']; ?>" <?= ($row['display'] == 1) ? 'checked' : ''; ?>></td>
+                        <td><input type="checkbox" name="del[]" id="" value="<?= $row['id']; ?>"></td>
+                        <td><input class='btn btn-primary' type="button" value="更新圖片"></td>
                         <!-- 表單內用button預設的效果為submit 所以用input較好 -->
+                        <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
                     </tr>
                 <?php
                 }
                 ?>
             </table>
             <div class="d-flex justify-content-between">
-                <div><input type="button" onclick="op('#cover','#cvr','title.php?do=title')" value="新增網站標題圖片"></div>
+                <div><input type="button" onclick="op('#cover','#cvr','title.php')" value="新增網站標題圖片"></div>
                 <!-- op=open -->
                 <div>
                     <input type="submit" value="修改確定">
