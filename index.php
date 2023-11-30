4ae06fc8c7587c95e6e10932295e912cb31bec8e
HelloCodeman <955khm@gmail.com>
2023-11-30 10:45:23 +0800

    建立作業基礎環境:建資料夾、下載bootstrap、建立導引頁

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>物件導向練習</title>
+    <link rel="stylesheet" href="/css/bootstrap.css">
+</head>
+<style>
+    ul li a {
+        text-decoration: none;
+    }
+
+    ul li a :hover {
+        text-decoration: underline;
+    }
+</style>
+
+<body>
+    <main class="container mt-5 text-center">
+        <ul class="list-group col-6 mx-auto">
+            <li class="list-group-item list-group-item-action"><a class="d-block" href="/q1/">題目一</a></li>
+            <li class="list-group-item list-group-item-action"><a class="d-block" href="/q1/">題目二</a></li>
+            <li class="list-group-item list-group-item-action"><a class="d-block" href="/q1/">題目三</a></li>
+            <li class="list-group-item list-group-item-action"><a class="d-block" href="/q1/">題目四</a></li>
+        </ul>
+    </main>
+    <script src="./js/bootstrap.js"></script>
+</body>
+
+</html>
