b29b91f42772a73629199e142789ee7931174918
HelloCodeman <955khm@gmail.com>
2023-12-01 14:28:04 +0800

    建立投票選項列表頁面

--- q2/index.php
+++ q2/index.php
@@ -37,7 +37,7 @@ include_once "db.php";
                         <td><?= $idx + 1; ?></td>
                         <td><?= $que['text']; ?></td>
                         <td><?= $que['count']; ?></td>
-                        <td><a class="btn btn-info" href="result.php?id=<?= $que['id']; ?>">投票結果</td>
+                        <td><a class='btn btn-info' href="result.php?id=<?= $que['id']; ?>">投票結果</a></td>
                         <td>
                             <a class="btn btn-warning" href="vote.php?id=<?= $que['id']; ?>">我要投票</a>
                         </td>
---
+++ q2/result.php
@@ -0,0 +1,41 @@
+<?php
+include_once "db.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Result</title>
+    <!-- <link rel="stylesheet" href="../css/bootstrap.css"> -->
+</head>
+
+<body>
+    <fieldset class="">
+        <legend> 目前位置：首頁 > 問卷調查 > 你最常做什麼運動來促進健康體能呢?
+        </legend>
+        <br>
+        <header class="">
+            你最常做什麼運動來促進健康體能呢?
+        </header>
+        <div>
+            <input type="radio">健走或爬樓梯、慢跑等較不受時間、場地限制的運動。
+        </div>
+        <div>
+            <input type="radio">仰臥起坐、抬腿及伏地挺身、伸展操、瑜珈等室內運動。
+        </div>
+        <div>
+            <input type="radio">球類運動、游泳、跳舞、騎腳踏車等加強心肺功能的運動。
+        </div>
+        <div>
+            <input type="radio">舉重鍛鍊、彈力帶、啞鈴等運用輔助器材鍛練肌耐力的運動。
+        </div>
+
+    </fieldset>
+    <!-- <script src="../js/jquery-3.4.1.min.js"></script> -->
+    <!-- <script src="../js/bootstrap.js"></script> -->
+</body>
+
+</html>
